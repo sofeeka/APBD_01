94cbf6e94466e1d0dad697983cd1f6211509a403
sofeeka <sofiiahrychukh@gmail.com>
2024-03-19 01:30:58 +0100

    Added static getAverage feature

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Modification 3");
+
+static double GetAverage(int[] arr)
+{
+    double sum = 0;
+    foreach (var number in arr)
+        sum += number;
+    return sum / arr.Length;
+} 
